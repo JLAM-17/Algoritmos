48cc1d83b267a9f2ba50f0b336864cb7bce90238
JuanL17 <56239786+JuanL17@users.noreply.github.com>
2020-01-23 10:00:56 -0500

    Update Taller1-2.cpp

--- Taller1-2.cpp
+++ Taller1-2.cpp
@@ -3,6 +3,12 @@
 #include <cmath>
 using namespace std;
 
+void imprimirVector(std::vector<int> &v){
+  for (unsigned int i = 0; i < v.size();i++){
+    std::cout << v[i] << '\n';
+  }
+}
+
 int main() {
   vector<int> vint;
   for (size_t i = 1; i < 11; i++){
@@ -13,8 +19,9 @@ int main() {
       vint.erase(vint.begin());
     }
   }
-  for (size_t i = 0; i < vint.size(); i++) {
+ for (size_t i = 0; i < vint.size(); i++) {
     std::cout << vint[i] << '\n';
   }
+  imprimirVector(vint);
   return 0;
 }
