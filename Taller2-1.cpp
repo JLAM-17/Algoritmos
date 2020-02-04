a0913fd3e9751af9bf6808f81fb49336294ff468
JuanL17 <56239786+JuanL17@users.noreply.github.com>
2020-02-04 10:15:02 -0500

    Add files via upload

---
+++ Taller2-1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+  map<int,string> m;
+  m.insert(pair<int,string>(1,"first"));
+  m[2] = "Hola";
+  m[1] = "Eliminado";
+  m[3] = "Tercero";
+  map<int,string> :: iterator it = m.begin();
+  while (it != m.end()){
+    std::cout << (*it).first << (*it).second << '\n';
+    it ++;
+  }
+  m.erase(2);
+  it = m.begin();
+  while (it != m.end()){
+    std::cout << (*it).first << (*it).second << '\n';
+    it ++;
+  }
+  std::cout << "Accediendo al valor de una llave: "  << m[1] << '\n';
+  std::cout << "Número de elementos: " << m.size() << '\n';
+  return 0;
+}
---
+++ Taller3-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <array>
+#include <vector>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  array<int,9> arr = {1,2,3,5,6,7,8,9,10};
+  std::vector<int> v;
+  for(unsigned int = 1; i > arr.size(); i++){
+    v.push_front(i);
+  }
+  while (v.size() > 1){
+    
+  }
+
+  return 0;
+}
